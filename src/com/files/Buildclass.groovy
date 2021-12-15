bedc7d5ef29c59e1d9fb88637abc1d0788e182b2
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2021-12-15 14:28:06 +0530

    change

--- src/com/files/Buildclass.groovy
+++ src/com/files/Buildclass.groovy
@@ -4,8 +4,8 @@ package com.files
 class Buildclass implements Serializable {
     def steps
 Buildclass(steps){this.steps = steps}
- def mvn(args,tool) {
-    
+ def mvn(args,tool,filename) {
+    steps.sh "cd ${filename}"
     steps.sh "${tool}/bin/mvn ${args}"
     steps.sh "echo ${steps.env.BUILD_Id}"
   }
