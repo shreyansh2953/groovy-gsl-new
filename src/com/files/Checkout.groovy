82b61af3b6864a508d19782de55907cdec6c9246
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2021-12-15 14:07:47 +0530

    change

--- src/com/files/Checkout.groovy
+++ src/com/files/Checkout.groovy
@@ -5,7 +5,7 @@ class Checkout implements Serializable{
     def steps
     Checkout(steps){this.steps = steps}
 
-    def checkout(repo){
+    def checkgit(repo){
       steps.sh "git clone ${repo}"
     }
 }
--- src/com/files/Sonarscans.groovy
+++ src/com/files/Sonarscans.groovy
@@ -3,7 +3,7 @@ package com.files
 
 class Sonarscans implements Serializable {
     def steps
-Buildclass(steps){this.steps = steps}
+Sonarscans(steps){this.steps = steps}
  def scan(args,tool) {
     steps.sh "${steps.tool 'myMaven'}/bin/mvn ${args}"
     
