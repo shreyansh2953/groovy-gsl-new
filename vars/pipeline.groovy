18b1fa40b2cb7e1090a6c22b003e272e7b804e2d
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2021-12-17 14:13:48 +0530

    change

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -1,28 +1,28 @@
 #!/usr/bin/env groovy
-
+import src.com.files.*;
 def call()
 {
 
 
    node{
-    def lib = library 'my-test-library'
-    def obj = lib.com.files;
+    // def lib = library 'my-test-library'
+    // def obj = lib.com.files;
     def mytool = tool 'myMaven'
     def repo_file="myfile"
     stage('checkout')
     {
          deleteDir()
-        def my_git = obj.Checkout.new(this)
+        def my_git = Checkout.new(this)
         my_git.checkgit("https://github.com/shreyansh2953/JavaCalculator.git")
     }
    stage('build')
     {
-        def my_build =obj.Buildclass.new(this)
+        def my_build =Buildclass.new(this)
         my_build.mavenBuild("clean",mytool,repo_file)
     }
     stage('scan')
     {
-        def my_scan = obj.Sonarscans.new(this)
+        def my_scan = Sonarscans.new(this)
         my_scan.scan("sonar:sonar",mytool)
     }
  }
