0ac9aed2ad86d8ffdc4bde81d50d332977ec9f58
shreyansh2953 <shreyanshnowlkha97@gmail.com>
2021-12-17 13:50:02 +0530

    change

--- src/com/files/Sonarscans.groovy
+++ src/com/files/Sonarscans.groovy
@@ -5,7 +5,10 @@ class Sonarscans implements Serializable {
     def steps
 Sonarscans(steps){this.steps = steps}
  def scan(args,tool) {
+    steps.withSonarQubeEnv(credentialsId: '7d113307-a77e-4c93-9e0c-a912ebd54db5') {
     steps.sh "${steps.tool 'myMaven'}/bin/mvn ${args}"
+    
+}
     
   }
     
